1b933369878cd5bfb097183eeb96ee8533eb2173
mouhammad-raddad <muhammadraddad2@gmail.com>
2021-08-30 18:51:54 +0300

    done

--- script.js
+++ script.js
@@ -22,28 +22,54 @@ function rama() {
             console.log("data", jsondata.results)
 
             for (let i = 0; i < jsondata.results.length; i++) {
-                inin += xdivstart + ximgstart + jsondata.results[i].image + ximgend
-                    + xh6start + jsondata.results[i].name + xh6end + xpstart + jsondata.results[i].species + xpend;
+                inin +=
+                    `<div class="d-flex flex-column">
+
+                <div class="p-2"></div>
+
+                <div class="p-2">
+                <div class="card">
+                    <div class="card-horizontal">
+                    <div class="img-square-wrapper">
+                    <img  src='${jsondata.results[i].image}' style="width:100%">
+                    </div>
+                        <div class=" card-body">
+                            <h4 id="bottombor" class="card-title">${jsondata.results[i].name}</h4>
+                            <hr>
+                            <p class="card-text">${jsondata.results[i].species}</p>
+                        </div>
+
+                        </div>
+
+                        </div>
+                        </div>
+                <div class="p-2"></div>
+                <br>`;
+
+                
+                
+
+
+
+
+
+
                 // inme += jsondata.results[i].name + "<br>";
-                // var div = document.createElement("div");
-                // inme[i] = jsondata.results[i].image
-                // var img = document.createElement('img');
-                // img.src = jsondata.results[i].image;
-                // var textNode = document.createTextNode(jsondata.results[i].name + " ");
-                // var textNode2 = document.createTextNode(jsondata.results[i].species);
-                // div.appendChild(textNode);
-                // div.appendChild(textNode2);
-                // document.body.appendChild(img);
-                // document.body.appendChild(div);
+                            // var div = document.createElement("div");
+                            // inme[i] = jsondata.results[i].image
+                            // var img = document.createElement('img');
+                            // img.src = jsondata.results[i].image;
+                            // var textNode = document.createTextNode(jsondata.results[i].name + " ");
+                            // var textNode2 = document.createTextNode(jsondata.results[i].species);
+                            // div.appendChild(textNode);
+                            // div.appendChild(textNode2);
+                            // document.body.appendChild(img);
+                            // document.body.appendChild(div);
+                            }
+                            console.log(inin);
+                            document.getElementById("puthere").innerHTML = inin;
+                            // document.body.innerHTML = inin;
+                            })
+                            .catch(error => console.log('error', error));
 
-
-            }
-            console.log(inin);
-
-
-            // document.getElementById("show").innerHTML = inin;
-            document.body.innerHTML = inin;
-        })
-        .catch(error => console.log('error', error));
-
-}
+                            }
